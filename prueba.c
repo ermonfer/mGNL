9c42a26c388b73e6bdf32d139fdf17ce80aba99b
Fernando Montero Erustes <fmontero@c1r11s5.42madrid.com>
2024-07-22 20:46:37 +0200

    El buffer hecho dinámico y factorizado

--- prueba.c
+++ prueba.c
@@ -1,22 +1,21 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
-
-//int main(void)
-//{   
-    //char *str1 = ft_strdup("patatas\nfritas");
-	//char *str = get_line(&str1);
-	//printf("%s", str);
-    //printf("%s", str1);
-    //free(str);
-    //free(str1);
-    //return 0;
-//}
-
 
 //! WARNING Main para probar 
 int main() {
-    int fd = open("texto.txt", O_RDONLY);
+    int fd = open("file", O_RDONLY);
     char *line;
+
+    while(1) {
+        line = get_next_line(fd);
+        if (!line) {
+            break ;
+        }
+        printf("%s", line);
+        free(line);
+    }
+    close(fd);
+    fd = open("texto.txt", O_RDONLY);
     while(1) {
         line = get_next_line(fd);
         if (!line) {
@@ -25,6 +24,6 @@ int main() {
         printf("%s", line);
         free(line);
     }
-    close(fd);
+    close(fd); 
     return 0;
 }
